5a25b31a82469d928829825ce4d0cac6cb057a39
digonil <diego.niles@hotmail.com>
2023-02-21 16:21:36 -0300

    Adicionado records e classes de controller, repository e de modelo da Entidade de médico

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,11 @@
+package med.voll.api.endereco;
+
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+}
