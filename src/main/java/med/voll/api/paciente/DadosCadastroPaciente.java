93ddb0b1a287a38ac6a04ea839f5f1512546ee9d
digonil <diego.niles@hotmail.com>
2023-02-21 22:02:53 -0300

    Adicionado funcionalidades para a entidade de Paciente

---
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,24 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        String cpf,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Valid
+        DadosEndereco endereco) {
+}
