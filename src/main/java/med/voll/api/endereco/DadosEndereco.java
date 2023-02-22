7356007a13d26b654aa1cae6e1010f2c4933a9f7
digonil <diego.niles@hotmail.com>
2023-02-21 21:30:24 -0300

    Criado a tabela de medicos no banco utilizando migrations

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,11 +1,19 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
         String numero,
+
         String complemento,
         String bairro,
         String cidade,
+        @NotBlank
         String uf,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
         String cep) {
 }
--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,11 +1,30 @@
 package med.voll.api.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
     private String logradouro;
+    private String bairro;
+    private String cep;
     private String numero;
     private String complemento;
-    private String bairro;
     private String cidade;
     private String uf;
-    private String cep;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+    }
 }
