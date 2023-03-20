24fdd58218feb0fa3953091c74cb76e26d012b08
Digonil <diego.niles@hotmail.com>
2023-03-19 22:09:16 -0300

    First commit

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -1,15 +1,18 @@
 package med.voll.api.paciente;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.DadosAtualizacaoMedico;
 
 @Entity(name = "Paciente")
 @Table(name = "pacientes")
 @Getter
 @EqualsAndHashCode(of = "{id}")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
