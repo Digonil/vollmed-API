4cc01c6bd6417383f2628162dd140a8ee884a3ca
Digonil <diego.niles@hotmail.com>
2023-03-25 23:27:48 -0300

    Realizado a configuração do Spring Security

---
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -0,0 +1,31 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.AutenticacaoService;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    public AuthenticationManager manager;
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        //Recebemos os dados do DTO e o Spring cria o seu próprio, que injetamos no autheticate para realizar a autenticação.
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+        // ResponseEntity para retornarmos 200 na response.
+        return ResponseEntity.ok().build();
+
+    }
+
+}
---
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    //Código com a lógica autenticação do projeto.
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return repository.findByLogin(login) ;
+
+    }
+
+
+
+}
