891fa65175ad736d8315fd330067890e10734b33
Digonil <diego.niles@hotmail.com>
2023-03-26 14:16:13 -0300

    Configurado token utilizando JWT

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.domain.usuario.AutenticacaoService;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,13 +20,17 @@ public class AutenticacaoController {
 
     @Autowired
     public AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         //Recebemos os dados do DTO e o Spring cria o seu próprio, que injetamos no autheticate para realizar a autenticação.
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
         // ResponseEntity para retornarmos 200 na response.
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 
     }
 
