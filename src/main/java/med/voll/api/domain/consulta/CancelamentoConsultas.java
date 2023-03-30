394505b8a9203102170a7ef60548f135632f471e
Digonil <diego.niles@hotmail.com>
2023-03-29 23:13:24 -0300

    Adicionado o recurso de agendar e cancelar consultas

---
+++ src/main/java/med/voll/api/domain/consulta/CancelamentoConsultas.java
@@ -0,0 +1,18 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CancelamentoConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+    public void cancelar(DadosCancelamento dados){
+
+        //CONTINUAR A LÓGICA DE CANCELAMENTO
+
+
+
+    }
+}
